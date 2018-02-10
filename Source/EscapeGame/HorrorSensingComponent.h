c5d3a336f635cf60c77c97e6e1bb8bb0fec509ec
Cameron <cameron.Peet@hotmail.com>
2018-02-10 21:14:11 +1300

    Build Failing : Before removing stuff

---
+++ Source/EscapeGame/HorrorSensingComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/PawnSensingComponent.h"
+#include "HorrorSensingComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEGAME_API UHorrorSensingComponent : public UPawnSensingComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
